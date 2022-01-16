f331d5b03226333f443443ea1acf64efe1c5596f
Nicolette Verlinden <nicole@nicole.moe>
2022-01-16 16:33:05 -0600

    Add main loop

--- src/header.asm
+++ src/header.asm
@@ -1,30 +1,31 @@
+header          .block
                 ;      123456789012345678901
 title           .text "TEST ROM             "
-
-info
 map_mode        .byte $30  ; lorom, fast
 chipset         .byte $00  ; ROM
 rom_size        .byte 5    ; 32 (1<<5) KB ROM
 ram_size        .byte 0    ; no RAM
-country_code    .byte $00  ; international
-developer_code  .byte $00  ; none
+country         .byte $00  ; international
+developer       .byte $00  ; none
 version         .byte $00  ; 1.0
 checksum        .word $ffff, $0000
+                .endblock
 
-vectors 
+vectors         .block
                 .addr dummy_handler
                 .addr dummy_handler
-v_816_cop       .addr dummy_handler
-v_816_brk       .addr dummy_handler
+cop_816         .addr dummy_handler
+brk_816         .addr dummy_handler
                 .addr dummy_handler
-v_816_nmi       .addr dummy_handler
+nmi_816         .addr vblank
                 .addr dummy_handler
-v_816_irq       .addr dummy_handler
-                .addr dummy_handler
-                .addr dummy_handler
-v_02_cop        .addr dummy_handler
+irq_816         .addr dummy_handler
                 .addr dummy_handler
                 .addr dummy_handler
-v_02_nmi        .addr dummy_handler
-v_02_reset      .addr reset
-v_02_irq_brk    .addr dummy_handler
+cop_02          .addr dummy_handler
+                .addr dummy_handler
+                .addr dummy_handler
+nmi_02          .addr dummy_handler
+reset_02        .addr reset
+irq_brk_02      .addr dummy_handler
+                .endblock
--- src/main.asm
+++ src/main.asm
@@ -1,24 +1,54 @@
                 .cpu "65816"
                 .include "../include/65816.inc"
                 .include "../include/snes.inc"
+                .include "../include/ram.inc"
                 .lorom
 
                 .org $808000
                 .include "reset.asm"
 
-                lda #rgb(0,0,31)    ; set color 0 to blue
+                ldy #$0f
+                sty ScrBrightness
+                sty INIDISP
+
+                ldy #$81
+                sty NMITIMEN
+
+main_loop       ; do processing here
+
+                jsr vsync
+                ldy #$8f            ; turn off screen for safety
+                sty INIDISP
+
+                lda StepCtL
                 a8
                 stz CGADD
                 sta CGDATA
                 xba
                 sta CGDATA
+                a16
 
-                lda #$0f            ; turn on screen
-                sta INIDISP
+                ldy ScrBrightness   ; turn screen back on
+                sty INIDISP
+                inc32 StepCtL
+                jmp main_loop
 
-forever         wai                 ; loop forever
-                bra forever
+vsync           ldy FrameCtL
+_spin           wai
+                cpy FrameCtL
+                beq _spin
+                rts
 
+                .databank ?
+                .dpage ?
+vblank          rep #%11111011      ; clear all flags except interrupt
+                .al
+                phb                 ; set data bank to $00
+                phk
+                plb
+                .databank $00
+                inc32 FrameCtL
+                plb
 dummy_handler   rti                 ; interrupt handler that does nothing
 zero            .byte $00           ; used to clear memory with DMAs
 
